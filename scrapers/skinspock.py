1ed82f2c5430fe43610a6ba292d2ab462e3d401b
nahuel <nahuel893@hotmaill.com>
2025-08-05 09:41:06 -0300

    Moved DataInventory class

--- scrapers/skinspock.py
+++ scrapers/skinspock.py
@@ -1,6 +1,10 @@
 import requests
 import os
 
+import pandas as pd
+import json
+
+
 class SkinspockAPI:
     """
     A class to interact with the Skinpock API for retrieving user inventory data.
@@ -89,4 +93,53 @@ class SkinspockAPI:
     def get_bloat_columns(self) -> list[str]:
         return self.bloat_columns
 
+
+class DataInventory:
+    """
+    Clase dedicada a la limpieza y transformacion de los datos obtenidos de las API de Skinspock.
+    """
+    def __init__(self, steamid) -> None:
+        self.steamid = steamid
+        self.skinspock = SkinspockAPI(steamid)
+        self.data = self.skinspock.get_inventory()
+        self.bloat_columns = self.skinspock.get_bloat_columns()
+
+        # Convert list to string and then to JSON, then to DataFrame
+        self.data = json.dumps(self.data)
+        self.data = json.loads(self.data)
+        self.df = pd.DataFrame(self.data)
+
+        self.price_date_name = "priceupdatedat"
+
+    def export_to_excel(self, filepath: str = "data.xlsx") -> None:
+        """
+        Exporta los datos a un archivo Excel en la ruta especificada.
+        """
+        self.df.to_excel(filepath, index=False)
+
+
+    def delete_bloat_columns(self) -> None:
+        """
+        Elimina columnas innecesarias del DataFrame.
+        """
+        self.df.drop(columns=self.bloat_columns, inplace=True, errors='ignore')
+
+    def transform_data(self) -> None:
+        """
+        Realiza transformaciones adicionales en los datos.
+        """
+        self.delete_bloat_columns()
+        print(self.df[self.price_date_name])  # type: ignore
+
+    def show_data(self) -> None:
+        """
+        Muestra los datos en la consola.
+        """
+        print(self.df.head())
+        print(self.df.columns)
+
+    def to_excel(self) -> None:
+        """
+        """
+        self.df.to_excel(r"../data/skinspock.xlsx", index=False)
 
